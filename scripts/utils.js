9d9fdbe12d70795efb0aa57b653bf74144930b19
madjarevicn <nikola.madjarevic@gmail.com>
2021-04-07 10:31:45 +0200

    Push script for storing deployed addresses.

---
+++ scripts/utils.js
@@ -0,0 +1,56 @@
+const fs = require('fs')
+const path = require('path')
+
+function getSavedContractAddresses(env) {
+    if(!env) {
+        env = 'local'
+    }
+
+    let json
+    try {
+        json = fs.readFileSync(path.join(__dirname, `../contract-addresses-${env}.json`))
+    } catch (err) {
+        json = '{}'
+    }
+    return JSON.parse(json)
+}
+
+function saveContractAddress(network, contract, address, env) {
+    if(!env) {
+        env = 'local'
+    }
+    const addrs = getSavedContractAddresses()
+    addrs[network] = addrs[network] || {}
+    addrs[network][contract] = address
+    fs.writeFileSync(path.join(__dirname, `../contract-addresses-${env}.json`), JSON.stringify(addrs, null, '    '))
+}
+
+function getSavedContractBytecodes(env) {
+    if(!env) {
+        env = 'local'
+    }
+    let json
+    try {
+        json = fs.readFileSync(path.join(__dirname, `../contract-bytecodes-${env}.json`))
+    } catch (err) {
+        json = '{}'
+    }
+    return JSON.parse(json)
+}
+
+function saveContractBytecode(network, contract, bytecode, env) {
+    if(!env) {
+        env = 'local'
+    }
+    const bytecodes = getSavedContractBytecodes()
+    bytecodes[network] = bytecodes[network] || {}
+    bytecodes[network][contract] = bytecode
+    fs.writeFileSync(path.join(__dirname, `../contract-bytecodes-${env}.json`), JSON.stringify(bytecodes, null, '    '))
+}
+
+module.exports = {
+    getSavedContractAddresses,
+    saveContractAddress,
+    getSavedContractBytecodes,
+    saveContractBytecode
+}
