9bb5d00f55a76e2f82db83a34348282b69523deb
Idan Portal <idan.4tal@gmail.com>
2021-04-12 15:27:50 +0300

    add: naming convensions.

--- contracts/ChainportBridgeBsc.sol
+++ contracts/ChainportBridgeBsc.sol
@@ -38,20 +38,21 @@ contract ChainportBridgeBsc{
     mapping(address => uint) public balancesByAddresses;
     mapping(address => address) public bep20ByErc20Address;
 
-    event TokensMinted(address ercToken, address sendTo, uint amount);
-    event TokensBurned(address ercToken, address sendTo, uint amount);
+    event TokensMinted(address tokenAddress, address issuer, uint amount);
+    event TokensBurned(address tokenAddress, address issuer, uint amount);
+    event TokenCreated(address newTokenAddress, address ethTokenAddress, string tokenName, string tokenSymbol);
 
 
-    function mintNewToken(uint256 amount, address erc20_address, string memory tokenName, string memory tokenSymbol) public {
+    function mintNewToken(address erc20_address, string memory tokenName, string memory tokenSymbol) public {
         BridgeMintableToken newToken = new BridgeMintableToken(tokenName, tokenSymbol);
 
         bep20ByErc20Address[address(erc20_address)] = address(newToken);
+        TokenCreated(address(newToken), erc20_address, tokenName, tokenSymbol);
     }
 
     function mintTokens(address token, address receiver, uint256 amount) public payable{
         BridgeMintableToken ercToken = BridgeMintableToken(token);
 
-        ercToken.transferFrom(address(msg.sender), address(this), amount);
 
         balancesByAddresses[address(ercToken)] = balancesByAddresses[address(ercToken)] + amount;
         ercToken.mint(receiver, amount);
