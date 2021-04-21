76298ddbfb6d1f49daacec5a24173669fc59daf9
madjarevicn <nikola.madjarevic@gmail.com>
2021-04-21 21:14:37 +0200

    Fix deployment script --> parse per network.

--- scripts/deploy_01.js
+++ scripts/deploy_01.js
@@ -1,19 +1,18 @@
 const hre = require("hardhat");
-const { hexify, toChainportDenomination: toChainportDenomination } = require('../test/setup');
+const { hexify, toChainportDenomination } = require('../test/setup');
 const { getSavedContractAddresses, saveContractAddress, getSavedContractBytecodes, saveContractBytecode } = require('./utils')
-const config = require('../deployments/deploymentConfig.json');
+let c = require('../deployments/deploymentConfig.json');
 
 async function main() {
     await hre.run('compile');
 
-    const ChainportCongress = await hre.ethers.getContractFactory("ChainportCongress");
+    const config = c[hre.network.name];
 
+    const ChainportCongress = await hre.ethers.getContractFactory("ChainportCongress");
     const chainportCongress = await ChainportCongress.deploy();
     await chainportCongress.deployed();
-
     console.log("ChainportCongress contract deployed to:", chainportCongress.address);
-    saveContractAddress(hre.network.name, 'ChainportCongress', chainportCongress.address);
-
+    saveContractAddress(hre.network.name, 'chainportCongress', chainportCongress.address);
 
     const ChainportCongressMembersRegistry = await hre.ethers.getContractFactory("ChainportCongressMembersRegistry");
     const chainportCongressMembersRegistry = await ChainportCongressMembersRegistry.deploy(
@@ -22,25 +21,20 @@ async function main() {
         chainportCongress.address
     );
     await chainportCongressMembersRegistry.deployed();
-
     console.log("ChainportCongressMembersRegistry contract deployed to:", chainportCongressMembersRegistry.address);
-    saveContractAddress(hre.network.name, 'ChainportCongressMembersRegistry', chainportCongressMembersRegistry.address);
+    saveContractAddress(hre.network.name, 'chainportCongressMembersRegistry', chainportCongressMembersRegistry.address);
 
 
-
-    const chainportToken = await hre.ethers.getContractFactory("ChainportToken");
-    const chainport = await chainportToken.deploy(
-        config.chainportTokenName,
-        config.chainportTokenSymbol,
-        toChainportDenomination(config.chainportTotalSupply.toString()),
+    const ChainportToken = await hre.ethers.getContractFactory("ChainportToken");
+    const chainportToken = await ChainportToken.deploy(
+        config.tokenName,
+        config.tokenSymbol,
+        toChainportDenomination(config.totalSupply.toString()),
         chainportCongress.address
     );
-    await chainport.deployed();
-
-    console.log("Chainport token deployed to:", chainport.address);
-
-    saveContractAddress(hre.network.name, 'ChainportToken', chainport.address);
-
+    await chainportToken.deployed();
+    console.log("Hord token deployed to:", chainportToken.address);
+    saveContractAddress(hre.network.name, 'hordToken', chainportToken.address);
 
     await chainportCongress.setMembersRegistry(chainportCongressMembersRegistry.address);
     console.log('ChainportCongress.setMembersRegistry(',chainportCongressMembersRegistry.address,') set successfully.');
