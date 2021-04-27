0b8ec0080a585a255a186965a3d2ff613b056275
markolazic01 <marko@2key.network>
2021-04-27 22:32:46 +0200

    [add-maintainers] -> Implement flow that allows congress to approve token release.

--- contracts/ChainportBridgeEth.sol
+++ contracts/ChainportBridgeEth.sol
@@ -12,6 +12,8 @@ contract ChainportBridgeEth is Initializable{
     mapping(address => bool) isProtected;
     // Mapping for setting time locks
     mapping(address => uint) timeLock;
+    // Mapping for congress approval
+    mapping(address => bool) isApprovedByCongress;
 
     uint private needsToBeDelayedThreshold; // Set value;
     uint private constant TIMELOCK = 2 days; // Length of time lock
@@ -21,27 +23,44 @@ contract ChainportBridgeEth is Initializable{
     event TokensUnfreezed(address tokenAddress, address issuer, uint amount);
     event TokensFreezed(address tokenAddress, address issuer, uint amount);
 
-    modifier onlyMaintainer{
+    modifier onlyMaintainer {
         require(MaintainersRegistry(MaintainersRegistryAddress).isMaintainer(msg.sender));
         _;
     }
 
-    function initialize(address _maintainersRegistryAddress) public initializer{
+    modifier onlyChainportCongress {
+        require(msg.sender == MaintainersRegistry(MaintainersRegistryAddress).chainportCongress());
+        _;
+    }
+
+    // Initialization function
+    function initialize(address _maintainersRegistryAddress) public initializer {
         MaintainersRegistryAddress = _maintainersRegistryAddress;
     }
 
-    // Lock mechanism
-    function lockAssets(address token) internal onlyMaintainer{
+    // Function used to mark assets as protected
+    function protectAssets(address token) internal onlyMaintainer {
         // Mark the assets
-        isProtected[token] = true;
+        isProtected[address(token)] = true;
     }
 
-    function setTimeLock(address token) internal onlyMaintainer{
+    // Function to set a time lock on specified asset
+    function setTimeLock(address token) internal onlyMaintainer {
         // Secure assets with time lock
-        timeLock[token] = block.timestamp + TIMELOCK;
+        timeLock[address(token)] = block.timestamp + TIMELOCK;
     }
 
-    function freezeToken(address token, uint256 amount) public payable{
+    // Function to approve token release
+    function approve(address token) internal onlyChainportCongress {
+        isApprovedByCongress[address(token)] = true;
+    }
+
+    // Function to reset asset state
+    function resetAssetState(address token) internal {
+        isApprovedByCongress[address(token)] = false;
+    }
+
+    function freezeToken(address token, uint256 amount) public payable {
         IERC20 ercToken = IERC20(token);
 
         ercToken.transferFrom(address(msg.sender), address(this), amount);
@@ -54,16 +73,22 @@ contract ChainportBridgeEth is Initializable{
     function releaseTokens(address token, address receiver, uint amount) public {
         require(balancesByAddresses[address(token)] >= amount, "ChainportBridgeEth :: Not enough funds");
 
-        // Check if assets are marked and if amount is big enough
-        if(isProtected[token] && amount >= needsToBeDelayedThreshold){
+        // Check if assets are protected, amount is considered important by its quantity and congress has not approved the release
+        if(isProtected[address(token)] && amount >= needsToBeDelayedThreshold && !isApprovedByCongress[address(token)]){
+            // Set the time lock
             setTimeLock(token);
         }
 
-        require(block.timestamp > timeLock[token], "ChainportBridgeEth :: Cannot perform transaction under time lock");
+        // Require that assets are either approved by the congress or time-lock is ended
+        require(block.timestamp > timeLock[address(token)] || isApprovedByCongress[address(token)], "ChainportBridgeEth :: Congress must approve token release first");
 
         IERC20 ercToken = IERC20(token);
         ercToken.transfer(address(receiver), amount);
         balancesByAddresses[address(token)] = balancesByAddresses[address(token)] - amount;
+
+        // This line is for securing that approval serves only for one transaction
+        resetAssetState(token);
+
         emit TokensUnfreezed(token, msg.sender, amount);
     }
 }
