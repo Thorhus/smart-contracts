70ae9fed3e9cd8adde5ed3358332ae7a182fc73d
markolazic01 <marko@2key.network>
2021-05-07 15:31:30 +0200

    [add-maintainers] -> Change nomenclature.

--- test/TestMaintainers.js
+++ test/TestMaintainers.js
@@ -2,72 +2,72 @@ const { expect } = require("chai");
 
 describe("MaintainersRegistry", function () {
 
-    let contractInstance;
+    let maintainersRegistryInstance;
     let maintainersRegistry;
     let chainportCongress;
-    let addr1, addr2;
     let maintainers;
+    let user1, user2;
 
     beforeEach(async function() {
         maintainersRegistry = await ethers.getContractFactory("MaintainersRegistry");
-        [chainportCongress, addr1, addr2, ...maintainers] = await ethers.getSigners();
+        [chainportCongress, user1, user2, ...maintainers] = await ethers.getSigners();
 
-        contractInstance = await maintainersRegistry.deploy();
+        maintainersRegistryInstance = await maintainersRegistry.deploy();
         for(let i = 0; i < maintainers.length; i++) {
             maintainers[i] = maintainers[i].address;
         }
     });
 
     it("Should initialize and make given addresses maintainers", async function () {
-        await contractInstance.initialize(maintainers, chainportCongress.address);
+        await maintainersRegistryInstance.initialize(maintainers, chainportCongress.address);
         let res;
         for(let i = 0; i < maintainers.length; i++) {
-            res = await contractInstance.isMaintainer(maintainers[i]);
+            res = await maintainersRegistryInstance.isMaintainer(maintainers[i]);
             expect(res).to.equal(true);
         }
-        expect(await contractInstance.chainportCongress()).to.equal(chainportCongress.address);
+        expect(await maintainersRegistryInstance.chainportCongress()).to.equal(chainportCongress.address);
     });
 
     describe("Maintainers Functions", function () {
         beforeEach(async function () {
-            await contractInstance.initialize(maintainers, chainportCongress.address);
+            await maintainersRegistryInstance.initialize(maintainers, chainportCongress.address);
         });
 
         describe("Adding a maintainer", function () {
             it("Should not let a non congress address add a maintainer", async function () {
-                await expect(contractInstance.connect(addr2).addMaintainer(addr1.address))
+                await expect(maintainersRegistryInstance.connect(user2).addMaintainer(user1.address))
                     .to.be.revertedWith("MaintainersRegistry: Restricted only to ChainportCongress");
             });
 
             it("Should add a maintainer (by congress)", async function () {
-                await contractInstance.connect(chainportCongress).addMaintainer(addr1.address);
-                expect(await contractInstance.isMaintainer(addr1.address)).to.equal(true);
+                await maintainersRegistryInstance.connect(chainportCongress).addMaintainer(user1.address);
+                expect(await maintainersRegistryInstance.isMaintainer(user1.address)).to.equal(true);
             });
 
             it("Should not add a same maintainer second time (by congress)", async function () {
-                await contractInstance.connect(chainportCongress).addMaintainer(addr1.address);
-                expect(await contractInstance.isMaintainer(addr1.address)).to.equal(true);
-                await expect(contractInstance.connect(chainportCongress).addMaintainer(addr1.address))
+                await maintainersRegistryInstance.connect(chainportCongress).addMaintainer(user1.address);
+                expect(await maintainersRegistryInstance.isMaintainer(user1.address)).to.equal(true);
+                await expect(maintainersRegistryInstance.connect(chainportCongress).addMaintainer(user1.address))
                     .to.be.revertedWith('MaintainersRegistry :: Address is already a maintainer');
             });
         });
 
         describe("Removing a maintainer", function () {
             it("Should not let a non congress address remove a maintainer", async function () {
-                await contractInstance.connect(chainportCongress).addMaintainer(addr1.address);
-                expect(await contractInstance.isMaintainer(addr1.address)).to.equal(true);
-                await expect(contractInstance.connect(addr2).removeMaintainer(addr1.address))
+                await maintainersRegistryInstance.connect(chainportCongress).addMaintainer(user1.address);
+                expect(await maintainersRegistryInstance.isMaintainer(user1.address)).to.equal(true);
+                await expect(maintainersRegistryInstance.connect(user2).removeMaintainer(user1.address))
                     .to.be.revertedWith("MaintainersRegistry: Restricted only to ChainportCongress");
             });
 
             it("Should remove a maintainer (by congress)", async function () {
-                await contractInstance.connect(chainportCongress).addMaintainer(addr1.address);
-                expect(await contractInstance.isMaintainer(addr1.address)).to.equal(true);
-                await contractInstance.connect(chainportCongress).removeMaintainer(addr1.address);
-                expect(await contractInstance.isMaintainer(addr1.address)).to.equal(false);
+                await maintainersRegistryInstance.connect(chainportCongress).addMaintainer(user1.address);
+                expect(await maintainersRegistryInstance.isMaintainer(user1.address)).to.equal(true);
+                await maintainersRegistryInstance.connect(chainportCongress).removeMaintainer(user1.address);
+                expect(await maintainersRegistryInstance.isMaintainer(user1.address)).to.equal(false);
             });
             it("Should not remove a nonexistent maintainer (by congress)", async function () {
-                await expect(contractInstance.connect(chainportCongress).removeMaintainer(addr1.address))
+                await expect(maintainersRegistryInstance.connect(chainportCongress).removeMaintainer(user1.address))
                     .to.be.revertedWith('MaintainersRegistry :: Address is not a maintainer');
             });
         });
