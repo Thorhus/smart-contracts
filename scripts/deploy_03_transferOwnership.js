ba1a1cfbe7b666839cd976717fb31d3729aab48c
markolazic01 <marko@2key.co>
2021-07-15 10:32:57 +0200

    Improved transfer ownership script.

--- scripts/deploy_03_transferOwnership.js
+++ scripts/deploy_03_transferOwnership.js
@@ -1,13 +1,15 @@
 const hre = require("hardhat");
 const { hexify, toChainportDenomination } = require('../test/setup');
-const { getSavedContractAddresses, saveContractAddress, saveContractProxies, getSavedContractProxies} = require('./utils')
+const { getSavedContractAddresses, saveContractAddress, saveContractProxies, getSavedContractProxies, getSavedContractProxyAbis} = require('./utils')
 let c = require('../deployments/deploymentConfig.json');
 
 async function main() {
     await hre.run('compile');
     const config = c[hre.network.name];
     const contracts = getSavedContractAddresses()[hre.network.name];
-    let admin = await upgrades.admin.getInstance();
+    const proxies = getSavedContractProxies()[hre.network.name];
+    const abi = getSavedContractProxyAbis()["ProxyAdmin"];
+    const admin = await hre.ethers.getContractAt(abi, proxies["ProxyAdmin"]);
     await admin.transferOwnership(contracts["ChainportCongress"]);
     console.log("Ownership successfully transfered to ChainportCongress");
 }
