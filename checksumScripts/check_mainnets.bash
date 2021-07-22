c016cd62fbf5b7cfc9cc26d5d874660b7200672f
madjarevicn <nikola.madjarevic@gmail.com>
2021-07-22 14:44:44 +0200

    Add verification and checkmainnets script

---
+++ checksumScripts/check_mainnets.bash
@@ -0,0 +1,3 @@
+npx hardhat run --network binanceMainnet checksumScripts/verifyOwnerships.js
+npx hardhat run --network mainnet checksumScripts/verifyOwnerships.js
+npx hardhat run --network polygonMainnet checksumScripts/verifyOwnerships.js
