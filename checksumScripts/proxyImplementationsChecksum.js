d2b7015c2979df6efc75b44aa987ba93da2bebe7
markolazic01 <marko@2key.co>
2021-07-27 14:01:41 +0200

    Improve proxy implementation checksum script.

--- checksumScripts/proxyImplementationsChecksum.js
+++ checksumScripts/proxyImplementationsChecksum.js
@@ -10,61 +10,33 @@ async function main() {
     const proxies = getSavedContractProxies()[hre.network.name]
     const abi = getSavedContractProxyAbis()["ProxyAdmin"]
     const admin = await hre.ethers.getContractAt(abi, proxies["ProxyAdmin"])
+    const standard = 42
 
-    const mainBridgeContractName = "ChainportMainBridge"
-    const sideBridgeContractName = "ChainportSideBridge"
-    let bridgeContractName
+    console.log("-".repeat(46*4));
+    console.log(
+        "|Proxy Name:"," ".repeat(standard - "Proxy Name:".length),"|",
+        "Local Address:"," ".repeat(standard - "Local Address:".length),"|",
+        "Proxy Address:"," ".repeat(standard - "Proxy Address:".length),"|",
+        "Is Synced"," ".repeat(standard - "Is Synced".length),"|"
+    )
+    console.log("-".repeat(46*4));
 
-    // Detecting which bridge is in use on selected network
-    try {
-        await hre.ethers.getContractAt(mainBridgeContractName, contracts[mainBridgeContractName])
-        bridgeContractName = mainBridgeContractName
-        console.log('Network is using main bridge.\n')
-    } catch (err) {
-        try {
-            await hre.ethers.getContractAt(sideBridgeContractName, contracts[sideBridgeContractName])
-            bridgeContractName = sideBridgeContractName
-            console.log('Network is using side bridge.\n')
-        } catch (err) {
-            console.log(err.message)
+    for(var proxy in proxies){
+        if(proxy !== "ProxyAdmin"){
+            let localImplementation = contracts[proxy];
+            let proxyImplementation = await admin.getProxyImplementation(proxies[proxy]);
+            let isSynced = false
+            if(localImplementation === proxyImplementation) isSynced = true
+            console.log(
+                "|"+proxy," ".repeat(standard - proxy.length),"|",
+                localImplementation," ".repeat(standard - localImplementation.length),"|",
+                proxyImplementation," ".repeat(standard - proxyImplementation.length),"|",
+                isSynced," ".repeat(standard - isSynced.toString().length),"|"
+            );
         }
     }
 
-    //console.log(proxyAdminAddress)
-    //console.log(proxyAdmin)
-
-    // Get implementations from json
-    const bridgeImplementation = contracts[bridgeContractName]
-    const validatorImplementation = contracts["Validator"]
-    const maintainersRegistryImplementation = contracts["MaintainersRegistry"]
-
-    // Get implementations from proxy admin
-    //console.log(getSavedContractProxyAbis()["ProxyAdmin"])
-    const adminBridgeImplementation = await admin.getProxyImplementation(proxies[bridgeContractName])
-    const adminValidatorImplementation = await admin.getProxyImplementation(proxies["Validator"])
-    const adminMaintainersRegistryImplementation = await admin.getProxyImplementation(proxies["MaintainersRegistry"])
-
-    // Compare implementation addresses
-    console.log("ProxyAdmin:"," ".repeat(41-"ProxyAdmin:".length),"Config:")
-    console.log(bridgeImplementation, adminBridgeImplementation)
-    assert.strictEqual(
-        bridgeImplementation,
-        adminBridgeImplementation,
-        "Bridge Not In Sync"
-    )
-    console.log(validatorImplementation, adminValidatorImplementation)
-    assert.strictEqual(
-        validatorImplementation,
-        adminValidatorImplementation,
-        "Validator Not In Sync"
-    )
-    console.log(maintainersRegistryImplementation, adminMaintainersRegistryImplementation)
-    assert.strictEqual(
-        maintainersRegistryImplementation,
-        adminMaintainersRegistryImplementation,
-        "MaintainersRegistry Not In Sync"
-    )
-    console.log("\nImplementations checksum complete - Implementations are in sync!")
+    console.log("-".repeat(46*4));
 }
 
 main()
