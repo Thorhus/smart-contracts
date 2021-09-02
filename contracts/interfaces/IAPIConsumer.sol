69c99f289642f935402cea6c465d3cc2cce225e6
markolazic01 <marko@2key.co>
2021-09-02 11:33:21 +0200

    Add function to makeCustomRequest to APIConsumer interface.

--- contracts/interfaces/IAPIConsumer.sol
+++ contracts/interfaces/IAPIConsumer.sol
@@ -7,5 +7,10 @@ pragma solidity ^0.6.12;
  */
 interface IAPIConsumer {
 	function getMainBridgeTokenSupply(address originalTokenAddress) external returns(bytes32);
+	function makeCustomRequest(
+		string memory requestString,
+		string memory pathString,
+		string memory method
+	) external returns(bytes32);
 	function getLatestResult() external view returns(bytes32);
 }
