e74d9f45b2b2d200d5f83cc7c1da7ed758c35788
markolazic01 <marko@2key.co>
2021-09-06 12:25:32 +0200

    Add TestFundManager.js file.

---
+++ test/TestFundManager.js
@@ -0,0 +1,11 @@
+const { expect } = require("chai");
+
+describe("Test Fund Manager", () => {
+
+    const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+
+    beforeEach(async () => {
+
+    });
+
+});
